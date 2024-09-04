73ad8d107ad1fdae6be9be65b096c73ba35b84f3
Luiz Augusto <zcarvalho722@gmail.com>
2024-09-03 23:57:36 -0300

    Initial commits

---
+++ src/routes.ts
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import multer from "multer";
+
+import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
+
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import uploadConfig from "./config/multer";
+
+const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
+
+//-- Rotas USER
+router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
+router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+//-- Rotas CATEGORY
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
+
+router.get("/category", isAuthenticated, new ListCategoryController().handle);
+
+//-- Rotas PRODUCT
+router.post(
+  "/product",
+  isAuthenticated,
+  upload.single("file"),
+  new CreateProductController().handle
+);
+
+export { router };
