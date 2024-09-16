bef6e66c62e770b62e302191b81c0b8658cd8c46
Luiz Augusto <zcarvalho722@gmail.com>
2024-09-16 20:08:45 -0300

    Acessando as fotos que enviar pela request

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import express, { Express, Request, Response, NextFunction } from "express";
 import "express-async-errors";
-import cors from 'cors';
+import cors from "cors";
+import path from "path";
 
 import { router } from "./routes";
 
 const app = express();
-
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.use(router);
+
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
@@ -19,9 +21,9 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   }
 
   return res.status(500).json({
-    status: 'error',
-    message: 'Internal server error.'
-  })
+    status: "error",
+    message: "Internal server error.",
+  });
 });
 
 app.listen(3333, () => console.log("Servidor online!!"));
